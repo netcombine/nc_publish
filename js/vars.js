7a7836dd1768fd34de27a1c2541f4be563c3b95b
NETCOMBINE-PC\NETCOMBINE <pgs@netcombine.com>
2017-07-26 10:17:25 +0900

    [n] 자료 업로드 - pgs

---
+++ js/vars.js
@@ -0,0 +1,8 @@
+'use strict';
+var nc_landing = {},
+	page_list = {},
+	component_list = [],
+	section_list = [],
+	resize_fn = [],//resize시 사용
+	scroll_fn = [],//scroll시 사용
+	wheel_fn = [];//wheel시 사용
