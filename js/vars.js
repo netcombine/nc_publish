4b79ddef83f1f13828bfaecb57cc9c3133754186
NETCOMBINE-PC\NETCOMBINE <pgs@netcombine.com>
2017-07-26 11:08:08 +0900

    [m] localhost셋팅 - pgs

--- js/vars.js
+++ js/vars.js
@@ -6,3 +6,14 @@ var nc_landing = {},
 	resize_fn = [],//resize시 사용
 	scroll_fn = [],//scroll시 사용
 	wheel_fn = [];//wheel시 사용
+
+
+
+//셋팅
+var local = true;//localhost 설정
+
+if(local){
+	if(location.protocol.indexOf('file') >= 0 || ( location.protocol.indexOf('http') <= -1 && location.href.indexOf('localhost') <= -1)){
+		location.href=location.href.replace(/.*nc_publish\/(.*)/,'http://localhost/$1');
+	}
+}
